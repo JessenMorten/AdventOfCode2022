9b7a21b57217efda1bdda28aa9f5d09c5d61e786
Morten Jessen <mortenjessen5d@gmail.com>
2022-12-01 22:41:49 +0100

    feat: day 1

---
+++ Days/Day01.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode2022.Days;
+
+internal class Day01 : IDay
+{
+    private readonly List<List<int>> _elves = new();
+
+    public void Setup()
+    {
+        File
+            .ReadAllLines("Input/01.txt")
+            .Select<string, int?>(line => int.TryParse(line, out var food) ? food : null)
+            .ToList()
+            .ForEach(food =>
+            {
+                if (food is null || !_elves.Any())
+                    _elves.Add(new());
+
+                if (food.HasValue)
+                    _elves.Last().Add(food.Value);
+            });
+    }
+
+    public object SolveA()
+    {
+        return _elves
+            .Select(elf => elf.Sum())
+            .Max();
+    }
+
+    public object SolveB()
+    {
+        return _elves
+             .Select(elf => elf.Sum())
+             .OrderByDescending(kcal => kcal)
+             .Take(3)
+             .Sum();
+    }
+}
